3a41b3709f9b75f44023d80d87f42e1ac121a463
zzzwco <zzzwco@outlook.com>
2022-06-29 10:13:19 +0800

    Add Queue-Imp

---
+++ Queue/Quene_Imp/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  Quene_Imp
+//
+//  Created by Bruce on 2022/6/29.
+//
+
+import Foundation
+
+/**
+ https://leetcode.cn/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+ */
+
+class CQueue {
+  
+  var inStack = [Int]()
+  var outStack = [Int]()
+  
+  init() {
+    
+  }
+  
+  func appendTail(_ value: Int) {
+    inStack.insert(value, at: 0)
+  }
+  
+  func deleteHead() -> Int {
+    if !outStack.isEmpty {
+      return outStack.removeLast()
+    }
+    if !inStack.isEmpty {
+      inStack.forEach {
+        outStack.append($0)
+        inStack.removeLast()
+      }
+      return outStack.removeLast()
+    }
+    return -1
+  }
+}
